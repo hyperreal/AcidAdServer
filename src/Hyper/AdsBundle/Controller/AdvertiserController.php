6af256b0dcb6a9b96bbb1ecf17bd294ee0e09970
fajka <fajka@hyperreal.info>
2012-08-26 13:09:06 +0200

    blueprint

--- src/Hyper/AdsBundle/Controller/AdvertiserController.php
+++ src/Hyper/AdsBundle/Controller/AdvertiserController.php
@@ -19,7 +19,7 @@ class AdvertiserController extends Controller
      * Lists all Advertiser entities.
      *
      * @Route("/", name="admin_advertiser")
-     * @Template()
+     * @Template()`
      */
     public function indexAction()
     {
