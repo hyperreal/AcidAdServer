64893005af50f2057142a15ca4981e80bf00c350
fajka <fajka@hyperreal.info>
2012-10-20 17:30:15 +0200

    appcache

--- web/app.php
+++ web/app.php
@@ -13,7 +13,7 @@ $loader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
