11a81ef5ecbde8635065e7b6d9a33dfbd028d772
fajka <fajka@hyperreal.info>
2013-01-30 18:38:45 +0100

    exchangecommand test

--- src/Wikp/PaymentMtgoxBundle/Util/StandardInputReader.php
+++ src/Wikp/PaymentMtgoxBundle/Util/StandardInputReader.php
@@ -4,8 +4,10 @@ namespace Wikp\PaymentMtgoxBundle\Util;
 
 class StandardInputReader
 {
+    // @codeCoverageIgnoreStart
     public function getStandardInput()
     {
         return file_get_contents('php://stdin');
     }
+    // @codeCoverageIgnoreEnd
 }
