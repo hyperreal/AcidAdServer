da4383960b6b9031166e7bce6ec704bc82e510ba
fajka <fajka@hyperreal.info>
2013-02-13 17:15:47 +0100

    some work

---
+++ src/Hyper/AdsBundle/Helper/BannerZoneCalendar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: pioter
+ * Date: 09.02.13
+ * Time: 12:58
+ * To change this template use File | Settings | File Templates.
+ */
+class BannerZoneCalendar
+{
+
+}
