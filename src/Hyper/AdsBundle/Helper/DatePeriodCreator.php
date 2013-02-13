54344b9936cbdb7c45c7d4d0a0fab3e3c09b9517
fajka <fajka@hyperreal.info>
2013-02-13 17:33:41 +0100

    datePeriodCreator test

--- src/Hyper/AdsBundle/Helper/DatePeriodCreator.php
+++ src/Hyper/AdsBundle/Helper/DatePeriodCreator.php
@@ -1,12 +1,94 @@
 <?php
+
+namespace Hyper\AdsBundle\Helper;
+
+use Hyper\AdsBundle\Exception\InvalidArgumentException;
+use Hyper\AdsBundle\Helper\DatePeriod;
+
 /**
- * Created by IntelliJ IDEA.
- * User: pioter
- * Date: 09.02.13
- * Time: 16:49
- * To change this template use File | Settings | File Templates.
+ * Purpose of this class is to split array of \DateTime objects that are expected to be almost consecutive
+ * (with some $interval) into array of \DatePeriod objects.
+ *
+ * For example:
+ *
+ * An array of objects representing all (but one) days in a month will be converted into the array of two \DatePeriod
+ * objects.
+ * [2013-02-01, 2013-02-02, ..., 2013-02-15, (there is no 2012-02-16!) 2013-02-17, 2012-02-18...2012-02-29] will be:
+ * [(2013-02-01, 2013-02-15), (2013-02-17, 2013-02-29)]
  */
 class DatePeriodCreator
 {
+    /** @var \DateTime[] */
+    private $dates;
 
+    /** @var \DateInterval */
+    private $interval;
+
+    /** @var \Hyper\AdsBundle\Helper\DatePeriod[] */
+    private $periods;
+
+    public function __construct(array $dates, \DateInterval $interval)
+    {
+        $minDate = min($dates);
+        /** @var $maxDate \DateTime */
+        $maxDate = max($dates);
+        if ($this->getIntervalInSeconds($maxDate->diff($minDate)) < $this->getIntervalInSeconds($interval)) {
+            throw new InvalidArgumentException('Interval should be lower than max($dates) - min($dates)');
+        }
+        $this->dates = array_values($dates);
+        $this->interval = $interval;
+    }
+
+    /**
+     * @return \Hyper\AdsBundle\Helper\DatePeriod[]
+     */
+    public function getPeriods()
+    {
+        if (empty($this->periods)) {
+            $this->createPeriods();
+        }
+
+        return $this->periods;
+    }
+
+    private function createPeriods()
+    {
+        $datesCount = count($this->dates);
+        $firsts = array($this->dates[0]);
+        $lasts = array();
+        for ($i = 0; $i < $datesCount; $i++) {
+
+            if ($i == $datesCount - 1) {
+                $lasts[] = $this->dates[$i];
+                break;
+            }
+
+            $nextDateByInterval = clone $this->dates[$i];
+            $nextDateByInterval->add($this->interval);
+            $nextInArray = $this->dates[$i + 1];
+            if ($nextInArray->format('Y-m-d') != $nextDateByInterval->format('Y-m-d')) {
+                $lasts[] = $this->dates[$i];
+                $firsts[] = $nextInArray;
+            }
+        }
+
+        $interval = $this->interval;
+        $this->periods =  array_map(
+            function (\DateTime $start, \DateTime $end) use ($interval) {
+                return new DatePeriod($start, $interval, $end);
+            },
+            $firsts,
+            $lasts
+        );
+    }
+
+    private function getIntervalInSeconds(\DateInterval $interval)
+    {
+        return $interval->s
+            + $interval->i * 60
+            + $interval->h * 3600
+            + $interval->d * 86400
+            + $interval->m * 2592000
+            + $interval->y * 946080000;
+    }
 }
--- src/Hyper/AdsBundle/Tests/Helper/DatePeriodCreatorTest.php
+++ src/Hyper/AdsBundle/Tests/Helper/DatePeriodCreatorTest.php
@@ -1,12 +1,87 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: pioter
- * Date: 10.02.13
- * Time: 09:33
- * To change this template use File | Settings | File Templates.
- */
-class DatePeriodCreatorTest
-{
 
+namespace Hyper\AdsBundle\Tests\Helper;
+
+use Hyper\AdsBundle\Helper\DatePeriodCreator;
+
+class DatePeriodCreatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Hyper\AdsBundle\Exception\InvalidArgumentException
+     */
+    public function testIntervalHigherThanDiffBetweenMaxAndMinDateCausesException()
+    {
+        new DatePeriodCreator($this->createDates(array(16)), new \DateInterval('P1Y'));
+    }
+
+    public function testGetPeriodsFromOneMonthWithOneDayBreak()
+    {
+        $interval = new \DateInterval('P1D');
+        $dates = $this->createDates(array(16));
+        $creator = new DatePeriodCreator($dates, $interval);
+        $periods = $creator->getPeriods();
+
+        $this->assertInternalType('array', $periods);
+        $this->assertCount(2, $periods);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[0]);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[1]);
+
+        $this->assertEquals('2013-03-01', $periods[0]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-15', $periods[0]->getEnd()->format('Y-m-d'));
+        $this->assertEquals('2013-03-17', $periods[1]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-30', $periods[1]->getEnd()->format('Y-m-d'));
+    }
+
+    public function testGetPeriodsFromOneMonthWithMoreThanOneBreak()
+    {
+        $interval = new \DateInterval('P1D');
+        $dates = $this->createDates(array(10, 15, 20, 21, 22));
+
+        $creator = new DatePeriodCreator($dates, $interval);
+        $periods = $creator->getPeriods();
+
+        $this->assertInternalType('array', $periods);
+        $this->assertCount(4, $periods);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[0]);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[1]);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[2]);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[3]);
+
+        $this->assertEquals('2013-03-01', $periods[0]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-09', $periods[0]->getEnd()->format('Y-m-d'));
+        $this->assertEquals('2013-03-11', $periods[1]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-14', $periods[1]->getEnd()->format('Y-m-d'));
+        $this->assertEquals('2013-03-16', $periods[2]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-19', $periods[2]->getEnd()->format('Y-m-d'));
+        $this->assertEquals('2013-03-23', $periods[3]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-30', $periods[3]->getEnd()->format('Y-m-d'));
+    }
+
+    public function testGetPeriodsFromMonthWithNoBrakes()
+    {
+        $interval = new \DateInterval('P1D');
+        $dates = $this->createDates(array());
+
+        $creator = new DatePeriodCreator($dates, $interval);
+        $periods = $creator->getPeriods();
+
+        $this->assertInternalType('array', $periods);
+        $this->assertCount(1, $periods);
+        $this->assertInstanceOf('Hyper\AdsBundle\Helper\DatePeriod', $periods[0]);
+
+        $this->assertEquals('2013-03-01', $periods[0]->getStart()->format('Y-m-d'));
+        $this->assertEquals('2013-03-30', $periods[0]->getEnd()->format('Y-m-d'));
+    }
+
+    private function createDates(array $exclude)
+    {
+        $dates = array();
+        for ($i = 1; $i < 31; $i++) {
+            if (!in_array($i, $exclude)) {
+                $dates[] = new \DateTime('2013-03-' . sprintf('%02d', $i));
+            }
+        }
+
+        return $dates;
+    }
 }
