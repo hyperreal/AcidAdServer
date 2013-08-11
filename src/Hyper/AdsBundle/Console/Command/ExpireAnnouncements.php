f0e46b24e8725a821d40ab32c12a318dd8b97f57
fajka <fajka@hyperreal.info>
2013-08-11 16:25:46 +0200

    expire announcements task refactor

--- src/Hyper/AdsBundle/Console/Command/ExpireAnnouncements.php
+++ src/Hyper/AdsBundle/Console/Command/ExpireAnnouncements.php
@@ -2,12 +2,22 @@
 
 namespace Hyper\AdsBundle\Console\Command;
 
+use Hyper\AdsBundle\Entity\Advertisement;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ExpireAnnouncements extends ContainerAwareCommand
 {
+    /** @var \DateTime */
+    private $numberOfDaysAgo;
+    /** @var boolean */
+    private $verbose;
+    /** @var \Doctrine\ORM\EntityManager */
+    private $entityManager;
+    /** @var $repository \Hyper\AdsBundle\Entity\AdvertisementRepository */
+    private $repository;
+
     protected function configure()
     {
         $this->setName('acid:task:expire-announcements')
@@ -19,36 +29,51 @@ class ExpireAnnouncements extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $numberOfDaysAgo = new \DateTime(
-            sprintf('now - %d days', $this->getContainer()->getParameter('announcement_expire_days'))
-        );
+        $this->setUp($input);
+        $announcements = $this->repository->getExpiredAnnouncements($this->numberOfDaysAgo);
 
-        /** @var $em \Doctrine\ORM\EntityManager */
-        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
-        /** @var $repository \Hyper\AdsBundle\Entity\AdvertisementRepository */
-        $repository = $em->getRepository('HyperAdsBundle:Advertisement');
-        $announcements = $repository->getExpiredAnnouncements($numberOfDaysAgo);
-        $verbose = $input->getOption('verbose');
-
-        if ($verbose && empty($announcements)) {
-            $output->writeln('There are no expired announcements');
+        if (empty($announcements)) {
+            $this->verboseWrite($output, 'There are no expired announcements');
             return;
         }
 
         foreach ($announcements as $announcement) {
-            /** @var $announcement \Hyper\AdsBundle\Entity\Announcement */
-            $announcement->setExpired(true);
-            $em->persist($announcement);
-            if ($verbose) {
-                $output->writeln(
-                    sprintf(
-                        'Announcement with id <info>%d</info> (<comment>%s</comment>) marked as expired',
-                        $announcement->getId(),
-                        $announcement->getTitle()
-                    )
-                );
-            }
+            $this->markAsExpiredAndPersist($output, $announcement);
         }
-        $em->flush();
+
+        $this->entityManager->flush();
+    }
+
+    private function verboseWrite(OutputInterface $output, $message)
+    {
+        if ($this->verbose) {
+            $output->writeln($message);
+        }
+    }
+
+    private function markAsExpiredAndPersist(OutputInterface $output, Advertisement $announcement)
+    {
+        $announcement->setExpired(true);
+        $this->entityManager->persist($announcement);
+        $this->verboseWrite(
+            $output,
+            sprintf(
+                'Announcement with id <info>%d</info> (<comment>%s</comment>) marked as expired',
+                $announcement->getId(),
+                $announcement->getTitle()
+            )
+        );
+    }
+
+    private function setUp(InputInterface $input)
+    {
+        $this->verbose = $input->getOption('verbose');
+        $this->entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $this->numberOfDaysAgo = $numberOfDaysAgo = new \DateTime(
+            sprintf('now - %d days', $this->getContainer()->getParameter('announcement_expire_days'))
+        );
+
+        $this->repository = $this->entityManager->getRepository('HyperAdsBundle:Advertisement');
     }
 }
